9836fdd6af05c47a83df7d8c822c551a4546c4cd
FarManCoder003 <codingfarman01@gmail.com>
2024-05-13 23:36:04 +0600

    May 13 2024

---
+++ src/components/ArrivalsItem.jsx
@@ -0,0 +1,40 @@
+import { FaHeart, FaShoppingCart } from "react-icons/fa";
+import { TfiReload } from "react-icons/tfi";
+
+const ArrivalsItem = ({ item }) => {
+  console.log(item);
+  return (
+    <div className="w-full lg:w-[24%] py-5">
+      <div className="relative group overflow-hidden">
+        <img
+          src={item.thumbnail}
+          className="lg:w-[350px] lg:h-[350px]"
+          alt=""
+        />
+        <div className="bg-white absolute left-0 h-[130px] duration-300 ease-in-out bottom-[-120px] w-full group-hover:bottom-[0px] flex items-center justify-end">
+          <ul className="pr-5">
+            <li className="flex items-center justify-end gap-x-4 text-[#767676] font-sans text-[16px] font-normal">
+              Add to Wish List <FaHeart className="text-[#262626]" />
+            </li>
+            <li className="flex items-center justify-end gap-x-4 py-2 text-[#767676] font-sans text-[16px] font-normal">
+              Compare <TfiReload className="text-[#262626]" />
+            </li>
+            <li className="flex items-center justify-end gap-x-4 text-[#262626] font-sans text-[16px] font-bold">
+              Add to Cart <FaShoppingCart />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex justify-between py-3">
+        <h2 className="text-[#262626] font-sans text-[20px] font-bold">
+          {item.title}
+        </h2>
+        <p className="text-[#767676] font-sans text-[16px] font-normal">
+          ${item.price}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ArrivalsItem;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 import "slick-carousel/slick/slick.css";
-import App from "./App.jsx";
-import "./index.css";
+import { ContextApi } from './components/ContextApi.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <ContextApi>
+      <App />
+    </ContextApi>
+  ,
+)
