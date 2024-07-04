f20c2b1966cb925558371b5fe2d62c739a144cfc
Farman <140155090+FarManCoder003@users.noreply.github.com>
2024-07-04 13:19:40 +0600

    July 4 2024

--- src/components/Ads.jsx
+++ src/components/Ads.jsx
@@ -3,6 +3,7 @@ import Ad2 from "../assets/Ad 2.png";
 import Ad3 from "../assets/Ad 3.png";
 import Flex from "./Flex";
 import Container from "./Container";
+import { Link } from "react-router-dom";
 
 const Ads = () => {
   return (
@@ -10,11 +11,17 @@ const Ads = () => {
       <Container>
         <Flex className="justify-between py-[15px] lg:py-[128px]">
           <div className="w-1/2">
+            <Link to={{ pathname: "https://dummyjson.com/products/category/smartphones" }}>
             <img src={Ad1} alt="" className="w-full h-full" />
+            </Link>
           </div>
           <div className="w-1/2  lg:ml-[40px] ml-[20px]">
+            <Link to={{ pathname: "https://dummyjson.com/products/category/furniture"}}>
             <img src={Ad2} alt="" className="w-full pb-[20px] lg:pb-[40px]" />
+            </Link>
+            <Link to={{ pathname: "https://dummyjson.com/products/category/furniture" }}>
             <img src={Ad3} alt="" className="w-full" />
+            </Link>
           </div>
         </Flex>
       </Container>
